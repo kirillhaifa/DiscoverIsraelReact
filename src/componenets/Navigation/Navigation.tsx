65cf9168b7c7b3e888648a1506fe3b6a55392ce9
kirillnesher <kiril.smushkin@wisdo.com>
2025-10-04 18:45:15 +0300

    haeder layout

--- src/componenets/Navigation/Navigation.tsx
+++ src/componenets/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 let classes = require('./Navigation.module.scss');
 import { translations } from '../../public/translations';
@@ -12,6 +12,7 @@ const Navigation = () => {
   const [user, loading, error] = useAuthState(auth);
   const { userData } = useSelector((state: RootState) => state.user);
   const { language } = useSelector((state: RootState) => state.language);
+  const [menuOpen, setMenuOpen] = useState(false);
   let languageShort = '';
   switch (language) {
     case 'ru':
@@ -27,33 +28,50 @@ const Navigation = () => {
       languageShort = 'en';
   }
 
-  return (
-      <ul className={classes.linkList}>
+  const navLinks = (
+    <>
+      <li className={classes.link}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>{translations.home[languageShort]}</Link>
+      </li>
+      <li className={classes.link}>
+        <Link to="/map" onClick={() => setMenuOpen(false)}>{translations.map[languageShort]}</Link>
+      </li>
+      <li className={classes.link}>
+        <Link to="/contacts" onClick={() => setMenuOpen(false)}>{translations.contacts[languageShort]}</Link>
+      </li>
+      <li className={classes.link}>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>{translations.aboutProject[languageShort]}</Link>
+      </li>
+      {user && userData?.role === 'admin' && (
         <li className={classes.link}>
-          <Link to="/">{translations.home[languageShort]}</Link>
-        </li>
-        {/* <li className={classes.link}>
-          <Link to="/unsafe-for-israelis">
-            {translations.unsafeForIsraelis[languageShort]}
+          <Link to="/admin" className={classes.adminLink} onClick={() => setMenuOpen(false)}>
+            {translations.adminPanel[languageShort]}
           </Link>
-        </li> */}
-        <li className={classes.link}>
-          <Link to="/map">{translations.map[languageShort]}</Link>
         </li>
-        <li className={classes.link}>
-          <Link to="/contacts">{translations.contacts[languageShort]}</Link>
-        </li>
-        <li className={classes.link}>
-          <Link to="/about">{translations.aboutProject[languageShort]}</Link>
-        </li>
-        {user && userData?.role === 'admin' && (
-          <li className={classes.link}>
-            <Link to="/admin" className={classes.adminLink}>
-              {translations.adminPanel[languageShort]}
-            </Link>
-          </li>
-        )}
-      </ul>
+      )}
+    </>
+  );
+
+  return (
+    <>
+      {/* Desktop navigation */}
+      <ul className={classes.linkList}>{navLinks}</ul>
+
+      {/* Mobile burger button */}
+      <button className={classes.menuButton} onClick={() => setMenuOpen(true)} aria-label="Открыть меню">
+        <span>☰</span>
+      </button>
+
+      {/* Mobile menu */}
+      {menuOpen && (
+        <nav className={classes.mobileMenu}>
+          <button className={"" + classes.closeButton} onClick={() => setMenuOpen(false)} aria-label="Закрыть меню">
+            ×
+          </button>
+          <ul>{navLinks}</ul>
+        </nav>
+      )}
+    </>
   );
 };
 
