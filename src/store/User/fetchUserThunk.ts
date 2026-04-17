33df37fe43c696a1c529d26cd5fc2c38daee6850
kirillhaifa <format3473@gmail.com>
2026-04-17 23:35:53 +0300

    fix build

--- src/store/User/fetchUserThunk.ts
+++ src/store/User/fetchUserThunk.ts
@@ -33,40 +33,4 @@ export const fetchUserData = createAsyncThunk<UserState, string>(
   }
 );
 
-// Асинхронный thunk для получения данных пользователя по userID
-export const fetchUserData = createAsyncThunk<UserState, string>(
-  'user/fetchUserData',
-  async (userID, { rejectWithValue }) => {
-    try {      
-      const userDocRef = doc(db, 'Users', userID);
-      const userSnapshot = await getDoc(userDocRef);
 
-      if (userSnapshot.exists()) {
-        const userData = userSnapshot.data();
-        
-        // Преобразуем joinDate в строку для сериализации
-        const result = {
-          userID: userData.userID,
-          name: userData.name || null,
-          surname: userData.surname || null,
-          premiumStatus: userData.premiumStatus || null,
-          email: userData.email || null,
-          profilePicture: userData.profilePicture || null,
-          joinDate: userData.joinDate ? userData.joinDate.toDate().toISOString() : null, // конвертация в строку
-          ratings: userData.ratings || null,
-          wishlist: userData.wishlist || null,
-          role: userData.role || null,
-          language: userData.language || null,
-          colorTheme: userData.colorTheme || null,
-        };
-        
-        return result;
-      } else {
-        return rejectWithValue('User not found');
-      }
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-      return rejectWithValue('Error fetching user data');
-    }
-  }
-);
